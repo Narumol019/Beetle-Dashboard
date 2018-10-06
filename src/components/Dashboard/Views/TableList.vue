93fd061b82695e3fc625b84706f6ecdac7eab64a
Narumol019 <s5830213019@phuket.psu.ac.th>
2018-10-07 00:59:06 +0700

    long

--- src/components/Dashboard/Views/TableList.vue
+++ src/components/Dashboard/Views/TableList.vue
@@ -1,9 +1,12 @@
 <template>
     <div class="row">
       <div class="col-md-12">
+          <div>
+              <input type="text" v-model="search" placeholder="Filter"/>
+            </div>
         <div class="card">
           <paper-table :title="table1.title" :sub-title="table1.subTitle" :data="table1.data" :columns="table1.columns">
-            
+
           </paper-table>
         </div>
       </div>
@@ -12,187 +15,148 @@
    
 </template>
 <script>
+  import {firestore} from './firebase.js'
   import PaperTable from 'components/UIComponents/PaperTable.vue'
-  const tableColumns = ['Beetle_No', 'Place', 'Date', 'Open', 'Colse', 'Rent', 'Total']
+  const tableColumns = ['Name', 'Place', 'Open', 'Colse', 'Price']
   const tableData = [{
-    beetle_no: 1,
+    name: 'beetle 1',
     place: 'Patong',
-    date: '2017-07-04',
     open: '14:30:05',
     colse: '14:32:55',
-    rent: 2.50,
-    total: 20
+    price: 20
   },
   {
-    beetle_no: 2,
+    name: 'beetle 2',
     place: 'Patong',
-    date: '2017-07-04',
     open: '14:30:05',
     colse: '14:32:55',
-    rent: 2.50,
-    total: 20
+    price: 20
   },
   {
-    beetle_no: 3,
+    name: 'beetle 3',
     place: 'Patong',
-    date: '2017-07-04',
     open: '14:30:05',
     colse: '14:32:55',
-    rent: 2.50,
-    total: 20
+    price: 20
   },
   {
-    beetle_no: 4,
+    name: 'beetle 4',
     place: 'Patong',
-    date: '2017-07-04',
     open: '14:30:05',
     colse: '14:32:55',
-    rent: 2.50,
-    total: 20
+    price: 20
   },
   {
-    beetle_no: 5,
+    name: 'beetle 5',
     place: 'Patong',
-    date: '2017-07-04',
     open: '14:30:05',
     colse: '14:32:55',
-    rent: 2.50,
-    total: 20
+    price: 20
   },
   {
-    beetle_no: 1,
+    name: 'beetle 1',
     place: 'Rawai',
-    date: '2017-08-06',
     open: '14:30:05',
     colse: '14:32:55',
-    rent: 2.50,
-    total: 20
+    price: 20
   },
   {
-    beetle_no: 2,
+    name: 'beetle 2',
     place: 'Rawai',
-    date: '2017-08-06',
     open: '14:30:05',
     colse: '14:32:55',
-    rent: 2.50,
-    total: 20
+    price: 20
   },
   {
-    beetle_no: 3,
+    name: 'beetle 3',
     place: 'Rawai',
-    date: '2017-08-06',
     open: '14:30:05',
     colse: '14:32:55',
-    rent: 2.50,
-    total: 20
+    price: 20
   },
   {
-    beetle_no: 4,
+    name: 'beetle 4',
     place: 'Rawai',
-    date: '2017-08-06',
     open: '14:30:05',
     colse: '14:32:55',
-    rent: 2.50,
-    total: 20
+    price: 20
   },
   {
-    beetle_no: 5,
+    name: 'beetle 5',
     place: 'Rawai',
-    date: '2017-08-06',
     open: '14:30:05',
     colse: '14:32:55',
-    rent: 2.50,
-    total: 20
+    price: 20
   },
   {
-    beetle_no: 1,
+    name: 'beetle 1',
     place: 'Kata',
-    date: '2018-09-11',
     open: '14:30:05',
     colse: '14:32:55',
-    rent: 2.50,
-    total: 20
+    price: 20
   },
   {
-    beetle_no: 2,
+    name: 'beetle 2',
     place: 'Kata',
-    date: '2018-09-11',
     open: '14:30:05',
     colse: '14:32:55',
-    rent: 2.50,
-    total: 20
+    price: 20
   },
   {
-    beetle_no: 3,
+    name: 'beetle 3',
     place: 'Kata',
-    date: '2018-09-11',
     open: '14:30:05',
     colse: '14:32:55',
-    rent: 2.50,
-    total: 20
+    price: 20
   },
   {
-    beetle_no: 4,
+    name: 'beetle 4',
     place: 'Kata',
-    date: '2018-09-11',
     open: '14:30:05',
     colse: '14:32:55',
-    rent: 2.50,
-    total: 20
+    price: 20
   },
   {
-    beetle_no: 5,
+    name: 'beetle 5',
     place: 'Kata',
-    date: '2018-09-11',
     open: '14:30:05',
     colse: '14:32:55',
-    rent: 2.50,
-    total: 20
+    price: 20
   },
   {
-    beetle_no: 1,
+    name: 'beetle 1',
     place: 'Nai-yang',
-    date: '2018-09-03',
     open: '14:30:05',
     colse: '14:32:55',
-    rent: 2.50,
-    total: 20
+    price: 20
   },
   {
-    beetle_no: 2,
+    name: 'beetle 2',
     place: 'Nai-yang',
-    date: '2018-09-03',
     open: '14:30:05',
     colse: '14:32:55',
-    rent: 2.50,
-    total: 20
+    price: 20
   },
   {
-    beetle_no: 3,
+    name: 'beetle 3',
     place: 'Nai-yang',
-    date: '2018-09-03',
     open: '14:30:05',
     colse: '14:32:55',
-    rent: 2.50,
-    total: 20
+    price: 20
   },
   {
-    beetle_no: 4,
+    name: 'beetle 4',
     place: 'Nai-yang',
-    date: '2018-09-03',
     open: '14:30:05',
     colse: '14:32:55',
-    rent: 2.50,
-    total: 20
+    price: 20
   },
   {
-    beetle_no: 5,
+    name: 'beetle 5',
     place: 'Nai-yang',
-    date: '2018-09-03',
     open: '14:30:05',
     colse: '14:32:55',
-    rent: 2.50,
-    total: 20
+    price: 20
   }]
 
   export default {
@@ -205,6 +169,11 @@
           columns: [...tableColumns],
           data: [...tableData]
         }
+      }
+    },
+    firestore () {
+      return {
+        place: firestore.collection('place')
       }
     }
 }
--- test/unit/specs/paper-table.spec.js
+++ test/unit/specs/paper-table.spec.js
@@ -2,184 +2,144 @@ import Vue from 'vue'
 import PaperTable from 'src/components/UIComponents/PaperTable.vue'
 
 const tableData = [{
-  beetle_no: 1,
+  name : 'beetle 1',
   place: 'Patong',
-  date: '2017-07-04',
   open: '14:30:05',
   colse: '14:32:55',
-  rent: 2.50,
-  total: 20
+  price: 20
 },
 {
-  beetle_no: 2,
+  name: 'beetle 2',
   place: 'Patong',
-  date: '2017-07-04',
   open: '14:30:05',
   colse: '14:32:55',
-  rent: 2.50,
-  total: 20
+  price: 20
 },
 {
-  beetle_no: 3,
+  name: 'beetle 3',
   place: 'Patong',
-  date: '2017-07-04',
   open: '14:30:05',
   colse: '14:32:55',
-  rent: 2.50,
-  total: 20
+  price: 20
 },
 {
-  beetle_no: 4,
+  name: ' ',
   place: 'Patong',
-  date: '2017-07-04',
   open: '14:30:05',
   colse: '14:32:55',
-  rent: 2.50,
-  total: 20
+  price: 20
 },
 {
-  beetle_no: 5,
+  name: 'beetle 5',
   place: 'Patong',
-  date: '2017-07-04',
   open: '14:30:05',
   colse: '14:32:55',
-  rent: 2.50,
-  total: 20
+  price: 20
 },
 {
-  beetle_no: 1,
+  name: 'beetle 1',
   place: 'Rawai',
-  date: '2017-08-06',
   open: '14:30:05',
   colse: '14:32:55',
-  rent: 2.50,
-  total: 20
+  price: 20
 },
 {
-  beetle_no: 2,
+  name: 'beetle 2',
   place: 'Rawai',
-  date: '2017-08-06',
   open: '14:30:05',
   colse: '14:32:55',
-  rent: 2.50,
-  total: 20
+  price: 20
 },
 {
-  beetle_no: 3,
+  name: 'beetle 3',
   place: 'Rawai',
-  date: '2017-08-06',
   open: '14:30:05',
   colse: '14:32:55',
-  rent: 2.50,
-  total: 20
+  price: 20
 },
 {
-  beetle_no: 4,
+  name: 'beetle 4',
   place: 'Rawai',
-  date: '2017-08-06',
   open: '14:30:05',
   colse: '14:32:55',
-  rent: 2.50,
-  total: 20
+  price: 20
 },
 {
-  beetle_no: 5,
+  name: 'beetle 5',
   place: 'Rawai',
-  date: '2017-08-06',
   open: '14:30:05',
   colse: '14:32:55',
-  rent: 2.50,
-  total: 20
+  price: 20
 },
 {
-  beetle_no: 1,
+  name: 'beetle 1',
   place: 'Kata',
-  date: '2018-09-11',
   open: '14:30:05',
   colse: '14:32:55',
-  rent: 2.50,
-  total: 20
+  price: 20
 },
 {
-  beetle_no: 2,
+  name: 'beetle 2',
   place: 'Kata',
-  date: '2018-09-11',
   open: '14:30:05',
   colse: '14:32:55',
-  rent: 2.50,
-  total: 20
+  price: 20
 },
 {
-  beetle_no: 3,
+  name: 'beetle 3',
   place: 'Kata',
-  date: '2018-09-11',
   open: '14:30:05',
   colse: '14:32:55',
-  rent: 2.50,
-  total: 20
+  price: 20
 },
 {
-  beetle_no: 4,
+  name: 'beetle 4',
   place: 'Kata',
-  date: '2018-09-11',
   open: '14:30:05',
   colse: '14:32:55',
-  rent: 2.50,
-  total: 20
+  price: 20
 },
 {
-  beetle_no: 5,
+  name: 'beetle 5',
   place: 'Kata',
-  date: '2018-09-11',
   open: '14:30:05',
   colse: '14:32:55',
-  rent: 2.50,
-  total: 20
+  price: 20
 },
 {
-  beetle_no: 1,
+  name: 'beetle 1',
   place: 'Nai-yang',
-  date: '2018-09-03',
   open: '14:30:05',
   colse: '14:32:55',
-  rent: 2.50,
-  total: 20
+  price: 20
 },
 {
-  beetle_no: 2,
+  name: 'beetle 2',
   place: 'Nai-yang',
-  date: '2018-09-03',
   open: '14:30:05',
   colse: '14:32:55',
-  rent: 2.50,
-  total: 20
+  price: 20
 },
 {
-  beetle_no: 3,
+  name: 'beetle 3',
   place: 'Nai-yang',
-  date: '2018-09-03',
   open: '14:30:05',
   colse: '14:32:55',
-  rent: 2.50,
-  total: 20
+  price: 20
 },
 {
-  beetle_no: 4,
+  name: 'beetle 4',
   place: 'Nai-yang',
-  date: '2018-09-03',
   open: '14:30:05',
   colse: '14:32:55',
-  rent: 2.50,
-  total: 20
+  price: 20
 },
 {
-  beetle_no: 5,
+  name: 'beetle 5',
   place: 'Nai-yang',
-  date: '2018-09-03',
   open: '14:30:05',
   colse: '14:32:55',
-  rent: 2.50,
-  total: 20
+  price: 20
 }
 ]
 
